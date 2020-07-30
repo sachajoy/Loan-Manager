e1a01f616dc8abca1e356571196ae6c4b97b8942
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-07-30 15:32:23 +0530

    added static and template in project

--- LoanManager/LoanManager/urls.py
+++ LoanManager/LoanManager/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('loan/', include('loan.urls')),
+    path('loan/', include('loan.urls', namespace='loan')),
     path('admin/', admin.site.urls),
 ]
--- LoanManager/loan/urls.py
+++ LoanManager/loan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import views
 
+app_name = 'loan'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
