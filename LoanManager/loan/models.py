92d3bca5249ed5318f4bf9c4613d7421f6126347
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-08-20 23:56:54 +0530

    Create taluka model

--- LoanManager/loan/models.py
+++ LoanManager/loan/models.py
@@ -12,3 +12,13 @@ class District(models.Model):
 
     def get_absolute_url(self):
         return reverse('loan:list-district')
+
+class Taluka(models.Model):
+    district_id = models.ForeignKey(District, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.district_id)
+
+    def get_absolute_url(self):
+        return reverse('loan:list-taluka')
