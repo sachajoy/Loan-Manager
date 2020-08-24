9be5aaaaaac839e78ed0707341b2320f4486ff6e
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-08-24 13:32:25 +0530

    Create model forms for Faliya and Village model

--- LoanManager/loan/forms/model_forms.py
+++ LoanManager/loan/forms/model_forms.py
@@ -13,3 +13,43 @@ class DisrictForm(ModelForm):
         self.fields['name'].widget.attrs.update(
             {'class': 'form-control form-control-user'}
         )
+
+
+class TalukaForm(ModelForm):
+    class Meta:
+        model = models.Taluka
+        fields = ['district_id', 'name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fields_name, fields in self.fields.items():
+            fields.widget.attrs.update(
+                {'class': 'form-control form-control-user'}
+            )
+
+
+class VillageForm(ModelForm):
+    class Meta:
+        model = models.Village
+        fields = ['taluka_id', 'name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fields_name, fields in self.fields.items():
+            fields.widget.attrs.update(
+                {'class': 'form-control form-control-user'}
+            )
+
+
+
+class FaliyaForm(ModelForm):
+    class Meta:
+        model = models.Faliya
+        fields = ['village_id', 'name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fields_name, fields in self.fields.items():
+            fields.widget.attrs.update(
+                {'class': 'form-control form-control-user'}
+            )
