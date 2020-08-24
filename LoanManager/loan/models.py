d958c68c495d5879e3d9502abfe2c65bf2dab091
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-08-24 13:31:27 +0530

    Created Model Faliya Village

--- LoanManager/loan/models.py
+++ LoanManager/loan/models.py
@@ -22,3 +22,26 @@ class Taluka(models.Model):
 
     def get_absolute_url(self):
         return reverse('loan:list-taluka')
+
+class Village(models.Model):
+    taluka_id = models.ForeignKey(Taluka, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.name, self.taluka_id, self.taluka_id.district_id)
+
+    def get_absolute_url(self):
+        return reverse('loan:list-village')
+
+class Faliya(models.Model):
+    village_id = models.ForeignKey(Village, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{} - {} - {} - {}".format(self.name,
+                                self.village_id,
+                                self.village_id.taluka_id,
+                                self.village_id.taluka_id.district_id )
+
+    def get_absolute_url(self):
+        return reverse('loan:list-faliya')
